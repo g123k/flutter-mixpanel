1b4162b705e81ddd1c49b070926a1dfe484bc8a8
Edouard Marquez <moi@edouard-marquez.me>
2018-04-18 16:02:45 +0200

    Fix the case of the plugin (h/m) files for iOS

--- ios/Classes/FlutterMixPanelPlugin.m
+++ ios/Classes/FlutterMixPanelPlugin.m
@@ -1,7 +1,7 @@
-#import "FluttermixpanelPlugin.h"
+#import "FlutterMixPanelPlugin.h"
 #import <fluttermixpanel/fluttermixpanel-Swift.h>
 
-@implementation FluttermixpanelPlugin
+@implementation FlutterMixPanelPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [SwiftFluttermixpanelPlugin registerWithRegistrar:registrar];
 }
--- ios/Classes/FluttermixpanelPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FluttermixpanelPlugin : NSObject<FlutterPlugin>
-@end
